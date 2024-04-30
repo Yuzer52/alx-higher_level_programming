b83f513304860c9cbe37fd95d35a8bd1a52f6921
yuzer52 <yuzer5252@gmail.com>
2024-04-30 03:35:22 -0700

    the last one

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
 """
-The Holberton School staff evaluates candidates
-applying for a back-end position with multiple technical challenges, like this one:
-"""
+import sys
 import requests
-from sys import argv
 
-if __name__ == '__main__':
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
     r = requests.get(url)
     commits = r.json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
