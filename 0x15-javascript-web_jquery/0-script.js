88ef2ee1e54371fdef733e47ee040c2974c4ec77
Yuzer52 <yuzer5252@gmail.com>
2024-05-30 19:17:31 +0000

    all

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the header element
+    var header = document.querySelector('header');
+    
+    // Check if the header element exists
+    if (header) {
+        // Change the text color to red
+        header.style.color = '#FF0000';
+    }
+});
+
